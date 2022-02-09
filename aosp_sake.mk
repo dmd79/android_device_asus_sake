4e01e57ce31a666ebfa5fdb900da69e917913837
dmd79 <davidevinavil@gmail.com>
2022-02-09 22:19:29 +0100

    sake: Ship with core Gapps

--- aosp_sake.mk
+++ aosp_sake.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Gapps
+WITH_CORE_GAPPS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
