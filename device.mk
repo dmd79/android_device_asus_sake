7430206655c26d29478221aadde1061788b49691
dmd79 <davidevinavil@gmail.com>
2022-02-09 19:53:24 +0100

    sake: Set TARGET_OTA_ASSERT_DEVICE

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := ZS590KS,ASUS_I006D
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
