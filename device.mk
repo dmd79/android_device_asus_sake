58af4ed89d76fab2e723e5c82d1af48ad5fe498c
dmd79 <davidevinavil@gmail.com>
2022-02-10 19:35:03 +0100

    sake: Build UdfpsResources

--- device.mk
+++ device.mk
@@ -82,7 +82,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.sake
+    android.hardware.biometrics.fingerprint@2.3-service.sake \
+    UdfpsResources
 
 # Bluetooth
 -include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
